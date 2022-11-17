75a087d79cba8ad5eba615acd5d6e6079b368273
a <19371836+insoxin@users.noreply.github.com>
2022-11-17 20:48:22 +0800

    Update ___ipv6_add_ipv4.sh

--- ___ipv6_add_ipv4.sh
+++ ___ipv6_add_ipv4.sh
@@ -9,7 +9,7 @@ echo -e "download wgcf wireguard-go"
 
 wget https://github.acglink.org/P3TERX/wireguard-go-builder/releases/download/0.0.20220316/wireguard-go-linux-amd64.tar.gz
 tar zxf wireguard-go-linux-amd64.tar.gz
-mv wireguard-go /usr/local/bin
+mv wireguard-go /usr/bin/
 rm -f wireguard-go-linux-amd64.tar.gz
 # wgcf
 wget https://github.acglink.org/ViRb3/wgcf/releases/download/v2.2.15/wgcf_2.2.15_linux_amd64 -O /usr/local/bin/wgcf
