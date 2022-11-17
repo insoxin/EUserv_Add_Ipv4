a5d3476fd49f29aa203ac31cfba1097fdd159a79
a <19371836+insoxin@users.noreply.github.com>
2022-11-17 18:30:49 +0800

    Update ___ipv6_add_ipv4.sh

--- ___ipv6_add_ipv4.sh
+++ ___ipv6_add_ipv4.sh
@@ -4,7 +4,7 @@ echo -e "更新安装环境"
 apt update
 UCF_FORCE_CONFOLD=1 DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y install curl
 apt install lsb-release iptables -y
-echo "deb http://mirrors.aliyun.com/ubuntu/ $(lsb_release -sc)-backports main" | sudo tee /etc/apt/sources.list.d/backports.list
+echo "deb http://deb.debian.org/debian/ $(lsb_release -sc)-backports main" | sudo tee /etc/apt/sources.list.d/backports.list
 apt update
 apt install -y net-tools iproute2 openresolv dnsutils
 apt install wireguard-tools --no-install-recommends
@@ -14,12 +14,12 @@ apt install wireguard-tools --no-install-recommends
 #download wgcf wireguard-go
 echo -e "download wgcf wireguard-go"
 # wireguard-go
-wget https://github.com/P3TERX/wireguard-go-builder/releases/download/0.0.20220316/wireguard-go-linux-amd64.tar.gz
+wget https://cdn.jsdelivr.net/gh/P3TERX/wireguard-go-builder/releases/download/0.0.20220316/wireguard-go-linux-amd64.tar.gz
 tar zxf wireguard-go-linux-amd64.tar.gz
 mv wireguard-go /usr/local/sbin
 rm -f wireguard-go-linux-amd64.tar.gz
 # wgcf
-wget https://github.com/ViRb3/wgcf/releases/download/v2.2.15/wgcf_2.2.15_linux_amd64 -O /usr/local/sbin/wgcf
+wget https://cdn.jsdelivr.net/gh/ViRb3/wgcf/releases/download/v2.2.15/wgcf_2.2.15_linux_amd64 -O /usr/local/sbin/wgcf
 chmod +x /usr/local/sbin/wgcf
 
 #添加权限
